c202bf140f23d72444e64e058943d0752cb16f08
Bayan Mardon <mardon.junior@gmail.com>
2025-09-19 01:20:01 -0400

    implement club routes

--- app/router/clubs.py
+++ app/router/clubs.py
@@ -9,10 +9,10 @@ router = APIRouter(
 
 async def get_players_by_position(scraper, club):
     page = await scraper.goto(f"/en/clubs/3/{club}/squad")
-    row_locator = page.locator("div.club-profile__squad-container")
-    await row_locator.wait_for(timeout=5000)
-    sections = await row_locator.all_inner_texts()
-    sections = [section.split("\n") for section in sections][0]
+    data_locator = page.locator("div.club-profile__squad-container")
+    await data_locator.wait_for(timeout=5000)
+    sections = await data_locator.all_inner_texts()
+    sections = sections[0].split("\n")
     counter = 0
     players = {'players': []}
     i = 0
@@ -27,8 +27,6 @@ async def get_players_by_position(scraper, club):
         })
         i += 3
     return players
-            
-        
 
 
 @router.get("/{name}/players")
@@ -45,10 +43,50 @@ async def get_club_players_by_position(name: str, position: str, scraper: Playwr
             filtered_players['players'].append(player)
     return filtered_players
 
-@router.get("/{name}/{stat_type}")
-async def get_club_stats(name: str, stat_type: str, scraper: PlaywrightService = Depends(get_scraper)):
-    pass
+async def get_all_club_stats(scraper, club):
+    page = await scraper.goto(f"/en/clubs/3/{club}/stats");
+    data_locator = page.locator("div.club-profile__stats")
+    await data_locator.wait_for(timeout=30000)
+    stats = await data_locator.all_inner_texts()
+    stats = stats[0].split("\n")
+    stats = [stat for stat in stats if stat != '']
+    return stats
 
-@router.get("/{name}/{stat_type}/{state_name}")
-async def get_club_sub_stats(name: str, stat_type: str, state_name: str, scraper: PlaywrightService = Depends(get_scraper)):
-    pass
+def format_stats(stats):
+    stats_dict = {
+        "General": {},
+        "Attack": {},
+        "Defence": {},
+        "Possession": {},
+        "Physical": {},
+        "Discipline": {}
+    }
+    cur_obj = "General" ## changed once we encounter the next heading
+    skip_cur = False
+    for i in range(len(stats)):
+        if skip_cur: ## skip the numbers
+            skip_cur = False
+            continue
+        ## if the next element is a string and the one after is an int, we're at a heading
+        elif stats[i] in set(stats_dict.keys()):
+            cur_obj = stats[i]
+            continue
+        stats_dict[cur_obj][stats[i]] = stats[i+1]
+        skip_cur = True
+    return stats_dict
+
+
+@router.get("/{name}/stats")
+async def get_club_overall_stats(name: str, scraper: PlaywrightService = Depends(get_scraper)):
+    stats = await get_all_club_stats(scraper, name)
+    return format_stats(stats)
+
+@router.get("/{name}/stats/{stat_type}")
+async def get_club_stats(name: str, stat_type: str, scraper: PlaywrightService = Depends(get_scraper)):
+    stats = await get_all_club_stats(scraper, name)
+    return format_stats(stats)[stat_type.title()]
+
+@router.get("/{name}/stats/{stat_type}/{stat_name}")
+async def get_club_sub_stats(name: str, stat_type: str, stat_name: str, scraper: PlaywrightService = Depends(get_scraper)):
+    stats = await get_all_club_stats(scraper, name)
+    return {str(stat_name.title()): format_stats(stats)[stat_type.title()][stat_name.title()]}
