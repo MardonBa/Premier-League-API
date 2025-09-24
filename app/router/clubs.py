7a46c8a9ba9e9e5aa679fa26c9ea78b1b14bc299
Bayan Mardon <mardon.junior@gmail.com>
2025-09-24 00:04:57 -0400

    abstract club route helper functions

--- app/router/clubs.py
+++ app/router/clubs.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from .deps import get_scraper
 from services.playwright_service import PlaywrightService
-from utils.clubs import get_players_by_position
+from utils.clubs import get_players_by_position, get_all_club_stats, format_stats
 
 router = APIRouter(
     prefix="/clubs",
@@ -21,38 +21,6 @@ async def get_club_players_by_position(name: str, position: str, scraper: Playwr
         if player['position'].lower() == position.lower():
             filtered_players['players'].append(player)
     return filtered_players
-
-async def get_all_club_stats(scraper, club):
-    page = await scraper.goto(f"/en/clubs/3/{club}/stats");
-    data_locator = page.locator("div.club-profile__stats")
-    await data_locator.wait_for(timeout=30000)
-    stats = await data_locator.all_inner_texts()
-    stats = stats[0].split("\n")
-    stats = [stat for stat in stats if stat != '']
-    return stats
-
-def format_stats(stats):
-    stats_dict = {
-        "General": {},
-        "Attack": {},
-        "Defence": {},
-        "Possession": {},
-        "Physical": {},
-        "Discipline": {}
-    }
-    cur_obj = "General" ## changed once we encounter the next heading
-    skip_cur = False
-    for i in range(len(stats)):
-        if skip_cur: ## skip the numbers
-            skip_cur = False
-            continue
-        elif stats[i] in set(stats_dict.keys()):
-            cur_obj = stats[i]
-            continue
-        stats_dict[cur_obj][stats[i]] = stats[i+1]
-        skip_cur = True
-    return stats_dict
-
 
 @router.get("/{name}/stats")
 async def get_club_overall_stats(name: str, scraper: PlaywrightService = Depends(get_scraper)):
--- utils/clubs.py
+++ utils/clubs.py
@@ -19,3 +19,34 @@ async def get_players_by_position(scraper, club):
         })
         i += 3
     return players
+
+async def get_all_club_stats(scraper, club):
+    page = await scraper.goto(f"/en/clubs/3/{club}/stats");
+    data_locator = page.locator("div.club-profile__stats")
+    await data_locator.wait_for(timeout=30000)
+    stats = await data_locator.all_inner_texts()
+    stats = stats[0].split("\n")
+    stats = [stat for stat in stats if stat != '']
+    return stats
+
+def format_stats(stats):
+    stats_dict = {
+        "General": {},
+        "Attack": {},
+        "Defence": {},
+        "Possession": {},
+        "Physical": {},
+        "Discipline": {}
+    }
+    cur_obj = "General" ## changed once we encounter the next heading
+    skip_cur = False
+    for i in range(len(stats)):
+        if skip_cur: ## skip the numbers
+            skip_cur = False
+            continue
+        elif stats[i] in set(stats_dict.keys()):
+            cur_obj = stats[i]
+            continue
+        stats_dict[cur_obj][stats[i]] = stats[i+1]
+        skip_cur = True
+    return stats_dict
